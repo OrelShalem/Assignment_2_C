6845d0bd7797f2f94873d5053eec1e65d0340ed3
OrelShalem <133002699+OrelShalem@users.noreply.github.com>
2024-02-07 18:49:03 +0200

    Update my_mat.h

--- my_mat.h
+++ my_mat.h
@@ -9,11 +9,16 @@
     #define SIZE  10
 // a constant definition exported by library:
     #define SIZE_VAL  100
+// a constant definition exported by library:
+    #define ITEMS  5
+// a constant definition exported by library:
+    #define WEIGHT_BAG  20
 
 void enterVal(int matrix[SIZE][SIZE], int values[]);
 int isPathExists(int i, int j, int matrix[SIZE][SIZE]);
 int bfs(int v, int dest, int n, int matrix[SIZE][SIZE]);
 int shortPath(int src, int dest,int matrix[SIZE][SIZE]);
-
+int knapSack(int weights[], int values[], int selected_bool[]);
+int max(int a, int b);
 
 #endif
